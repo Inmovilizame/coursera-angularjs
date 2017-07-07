13c1d645d659b61266431c431a3ee6e837b0ec48
Roi <roi.rodriguez@xercode.es>
2017-07-07 18:38:12 +0200

    Second phase

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,22 +1,64 @@
 (function () {
     'use strict';
-    
+
     angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+    .directive('foundItems', FoundItems)
     ;
 
-    NarrowItDownController.$inject = [];
-    function NarrowItDownController() {
+    function FoundItems() {
+        var ddo = {
+            restrict: 'E',
+            templateUrl: 'foundItems.html',
+            scope: {
+                items: '<foundItems',
+                remove: '&onRemove'
+            }
+        }; 
 
+        return ddo;
     }
 
-    MenuSearchService.$inject = ['$http'];
-    function MenuSearchService($http) {
+    NarrowItDownController.$inject = ['MenuSearchService']
+    function NarrowItDownController(MenuSearchService) {
+        var narrowit = this;
+        
+        narrowit.searchterm = '';
+
+        narrowit.found = [];
+
+        narrowit.filtermenu = function () {
+            var promise = MenuSearchService.getMatchedMenuItems(narrowit.searchterm);
+            promise.then(function (filteredItems) {
+                console.log("We have found ", filteredItems.length, " items");
+                narrowit.found = filteredItems;
+            });
+        }
+
+        narrowit.removeElement = function(index) {
+            narrowit.found.splice(index, 1);
+        }
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath']
+    function MenuSearchService($http, ApiBasePath) {
         var search = this;
 
         search.getMatchedMenuItems = function (searchTerm) {
+            console.log("We are looking for: ", searchTerm);
+            return $http({
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json"),
+            }).then(function (result) {
+                var menuItems = result.data.menu_items;
+                var foundItems = menuItems.filter(function (value){
+                    return -1 !== value.description.indexOf(searchTerm);
+                });
 
-        };
+                return foundItems;
+            });
+        }
     }
 })();
